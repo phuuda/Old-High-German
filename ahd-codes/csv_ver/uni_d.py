b55b3b79fee76841677b5b12e542579584cb81ef
Aysinm <tonik577@gmail.com>
2017-03-04 15:27:46 +0300

    Update uni_d.py

--- ahd-codes/csv_ver/uni_d.py
+++ ahd-codes/csv_ver/uni_d.py
@@ -11,16 +11,6 @@ def all_reps(filename, reps):
     f.write(text)
     f.close()
 
-'''
-data = {
-    'isidor.txt':{'dh': 'd', 'Dh': 'D'},
-    'tatian.txt':{' th': ' d', ' Th': ' D', 'Th': 'D', '\nth': '\nd'},
-    'hildebrandslied.txt':{'ð': 'd', 'Ð': 'D'}
-    # ����� ��� ���������� ����� ������� � ���������� ����. �����������
-    # (?) � ���������� �������� ��������� ���������, ��� ������ ��� ����-��� ������ �����
-}
-'''
-
 if __name__ == '__main__':
     for file_name, rule in get_data('data_table.csv').items():
         all_reps(file_name, {rule.start: rule.end})
