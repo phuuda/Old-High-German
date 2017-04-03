f2dabf4ee7e089fb70d43d1509d000f71bd1d734
phuuda <phuuda@gmail.com>
2017-04-03 23:31:38 +0300

    code edit

--- ahd-codes/csv_ver/uni_d.py
+++ ahd-codes/csv_ver/uni_d.py
@@ -1,15 +1,21 @@
 from rules import get_data
+import os # new stuff
 
 def all_reps(filename, reps):
-    f = open(filename, 'r', encoding='utf-8')
-    text = f.read()
-    print(text)
-    f.close()
-    for k, v in reps.items():
-        text = text.replace(k, v)
-    f = open(filename.replace('.txt', '_result.txt'), 'w', encoding='utf-8')
-    f.write(text)
-    f.close()
+
+    for root, dirs, files in os.walk('.'): # new stuff
+        for i in files:
+            if i == filename:
+
+                f = open(filename, 'r', encoding='utf-8')
+                text = f.read()
+                print(text)
+                f.close()
+                for k, v in reps.items():
+                    text = text.replace(k, v)
+                f = open(filename.replace('.txt', '_result.txt'), 'w', encoding='utf-8')
+                f.write(text)
+                f.close()
 
 if __name__ == '__main__':
     for file_name, rule in get_data('data_table.csv').items():
