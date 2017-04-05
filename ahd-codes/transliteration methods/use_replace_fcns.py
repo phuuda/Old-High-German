92338cda571483123da744f5fb61c8e3094f6759
phuuda <phuuda@gmail.com>
2017-04-05 23:13:36 +0300

    initial commit

---
+++ ahd-codes/transliteration methods/use_replace_fcns.py
@@ -0,0 +1,38 @@
+# WIP
+
+from rules import get_data
+from replace_fcns import begin_of_word, after_something, before_r_cons, any_place
+
+def txt_to_word_list(txt_file):
+    f = open(txt_file, 'r', encoding = 'utf-8')
+    text = f.read()
+    word_list = []
+
+    for x in text:
+        word_list.append(x)
+    f.close()
+
+    return word_list
+
+rules = get_data('data_table.csv')
+
+def apply_rules(text_file, word, rules):
+    changed_word = word
+    for rule in rules:
+        if text_file == rule['text']:
+            if 'после гл.' in rule['position']:
+                changed_word = after_something(changed_word, rule)
+                
+            if 'после согл.' in rule['position']:
+                changed_word = after_something(changed_word, rule)
+                
+            if 'перед сочетанием r+согл.' in rule['position']:
+                changed_word = before_r_cons(changed_word, rule)
+                
+            if 'в начале слова' in rule['position']:
+                changed_word = begin_of_word(changed_word, rule)
+                
+            if 'любая' in rule['position']:
+                changed_word = any_place(changed_word, rule)
+
+    return changed_word
